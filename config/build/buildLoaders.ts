73b05f21d966721e220276edb7aa54f59b51a4db
alexandru997 <52069086+alexandru997@users.noreply.github.com>
2022-09-15 19:07:28 +0300

    [add] react on project, and install scss loader

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -1,11 +1,21 @@
 import webpack from "webpack";
 
 export function buildLoaders(): webpack.RuleSetRule[] {
-
-    const loaders = {
+    const scssLoaders = {
+            test: /\.s[ac]ss$/i,
+            use: [
+                // Creates `style` nodes from JS strings
+                "style-loader",
+                // Translates CSS into CommonJS
+                "css-loader",
+                // Compiles Sass to CSS
+                "sass-loader",
+            ],
+        }
+        const loaders = {
         test: /\.tsx?$/,
         use: 'ts-loader',
         exclude: /node_modules/,
     }
-    return [loaders]
+    return [loaders, scssLoaders]
 }
--- src/index.ts
+++
@@ -1,5 +0,0 @@
-import {someFn} from "./test";
-
-someFn(997);
-
-document.body.innerHTML=`<div style="background: red">Hello World!!!</div>`
