351706572121f885d860db04e8e13fb23b3a8182
alexandru997 <52069086+alexandru997@users.noreply.github.com>
2022-09-15 22:38:10 +0300

    [add] react-router-dom, add lazy loading, separate in small bundle chunck,add routes for page

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,21 @@
-import React from 'react';
-import {Counter} from "./Components/Counter";
+import React, {Suspense} from 'react';
+import {Link} from 'react-router-dom';
+import {Routes, Route} from 'react-router-dom';
 import './index.scss'
+import {AboutPageAsync} from "./pages/AboutPage/AboutPage.async";
+import {MainPageAsync} from "./pages/MainPage/MainPage.async";
 
 const App = () => {
     return (
         <div className='app'>
-            Hello project
-            <Counter/>
+            <Link to={'/'}>Home</Link>
+            <Link to={'about'}>About</Link>
+            <Suspense fallback={<div>Loading...</div>}>
+                <Routes>
+                    <Route path={'about'} element={<AboutPageAsync/>}/>
+                    <Route path={'/'} element={<MainPageAsync/>}/>
+                </Routes>
+            </Suspense>
         </div>
     );
 };
