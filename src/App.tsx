f460c2e86a1015cb385abcc9d9235b47fded8161
alexandru997 <52069086+alexandru997@users.noreply.github.com>
2022-09-19 22:42:22 +0300

    [add] create useContext, am move theme into  localstorage and context

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,19 @@
 import React, {Suspense} from 'react';
 import {Link} from 'react-router-dom';
 import {Routes, Route} from 'react-router-dom';
-import './index.scss'
+import './styles/index.scss'
 import {AboutPageAsync} from "./pages/AboutPage/AboutPage.async";
 import {MainPageAsync} from "./pages/MainPage/MainPage.async";
+import {useTheme} from "./theme/useTheme";
+
 
 const App = () => {
+
+    const {theme, toggleTheme} = useTheme()
+
     return (
-        <div className='app'>
+        <div className={`app ${theme}`}>
+            <button onClick={toggleTheme}>Theme</button>
             <Link to={'/'}>Home</Link>
             <Link to={'about'}>About</Link>
             <Suspense fallback={<div>Loading...</div>}>
