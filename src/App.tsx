4fa68af2551a1002808877d548dc615eb46efc69
alexandru997 <52069086+alexandru997@users.noreply.github.com>
2022-09-19 23:02:47 +0300

    [add] create classnames function, and add file gitignore

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import './styles/index.scss'
 import {AboutPageAsync} from "./pages/AboutPage/AboutPage.async";
 import {MainPageAsync} from "./pages/MainPage/MainPage.async";
 import {useTheme} from "./theme/useTheme";
+import {classNames} from "./helpers/classnNames/classNames";
 
 
 const App = () => {
@@ -12,7 +13,7 @@ const App = () => {
     const {theme, toggleTheme} = useTheme()
 
     return (
-        <div className={`app ${theme}`}>
+        <div className={classNames('app', {}, [theme])}>
             <button onClick={toggleTheme}>Theme</button>
             <Link to={'/'}>Home</Link>
             <Link to={'about'}>About</Link>
