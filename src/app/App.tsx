74eb53450b9ce49fb344065d861d7032cd3b8294
alexandru997 <52069086+alexandru997@users.noreply.github.com>
2022-09-27 22:03:23 +0300

    [add] collapse sidebar, and styling

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -7,6 +7,7 @@ import {classNames} from "shared/lib/classnNames/classNames";
 import {useTheme} from "app/providers/ThemeProvider";
 import {AppRouter} from "app/providers/router";
 import {Navbar} from "widgets/Navbar";
+import {Sidebar} from "widgets/Sidebar";
 
 
 const App = () => {
@@ -16,7 +17,10 @@ const App = () => {
     return (
         <div className={classNames('app', {}, [theme])}>
             <Navbar/>
-            <AppRouter/>
+            <div className='content-page'>
+                <Sidebar/>
+                <AppRouter/>
+            </div>
         </div>
     );
 };
--- src/app/providers/router/ui/AppRouter.tsx
+++ src/app/providers/router/ui/AppRouter.tsx
@@ -13,7 +13,10 @@ const AppRouter = () => {
                         path={path}
                         element={(
                             <Suspense fallback={<div>Loading ...</div>}>
+                                <div className='page-wrapper'>
+
                                 {element}
+                                </div>
                             </Suspense>
                         )}
                     />
