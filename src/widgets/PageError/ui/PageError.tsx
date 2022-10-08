8fdeddf79ab16d825ec6f6c5e4183281f4a6a7c9
alexandru997 <52069086+alexandru997@users.noreply.github.com>
2022-10-08 16:31:39 +0300

    [add] error boundary

---
+++ src/widgets/PageError/ui/PageError.tsx
@@ -0,0 +1,25 @@
+import { classNames } from 'shared/lib/classnNames/classNames';
+import { useTranslation } from 'react-i18next';
+import { Button } from 'shared/ui/Button/ui/Button';
+import cls from './PageError.module.scss';
+
+interface PageErrorProps{
+    className?:string
+}
+
+const reloadPage = () => {
+    // eslint-disable-next-line no-restricted-globals
+    location.reload();
+};
+
+export const PageError = ({ className }:PageErrorProps) => {
+    const { t } = useTranslation();
+    return (
+        <div className={classNames(cls.PageError, {}, [className])}>
+            <p>{t('SOMETHING_WENT_WRONG')}</p>
+            <Button onClick={reloadPage}>
+                {t('UPDATE_PAGE')}
+            </Button>
+        </div>
+    );
+};
