a4c49f6f79537e6163be8067fca34008da2b1de0
alexandru997 <52069086+alexandru997@users.noreply.github.com>
2022-10-10 21:42:11 +0300

    [add] absolute import for test

--- src/shared/lib/classnNames/classNames.test.ts
+++ src/shared/lib/classnNames/classNames.test.ts
@@ -1,4 +1,4 @@
-import { classNames } from './classNames';
+import { classNames } from 'shared/lib/classnNames/classNames';
 
 describe('classNames', () => {
     test('with only first param', () => {
