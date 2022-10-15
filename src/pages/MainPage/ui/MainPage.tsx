3542d69c522d17aab1b90d5af8c0cef32d02bfb0
alexandru997 <52069086+alexandru997@users.noreply.github.com>
2022-10-15 13:01:51 +0300

    [fix] translation and hidden button with error

--- src/pages/MainPage/ui/MainPage.tsx
+++ src/pages/MainPage/ui/MainPage.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import { useTranslation } from 'react-i18next';
-import { BugButton } from 'app/providers/ErrorBoundary';
+// import { BugButton } from 'app/providers/ErrorBoundary';
 
 const MainPage = () => {
     const { t } = useTranslation();
     return (
         <div>
-            <BugButton />
+            {/* <BugButton /> */}
             {t('MAIN_PAGE')}
         </div>
     );
--- src/widgets/Navbar/ui/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -1,17 +1,21 @@
 import React from 'react';
 import { classNames } from 'shared/lib/classnNames/classNames';
 import { AppLink, AppLinkTheme } from 'shared/ui/AppLink/AppLink';
+import { useTranslation } from 'react-i18next';
 import cls from './Navbar.module.scss';
 
 interface NavbarProps {
     className?: string
 }
 
-export const Navbar = ({ className }: NavbarProps) => (
-    <div className={classNames(cls.Navbar, {}, [className])}>
-        <div className={cls.links}>
-            <AppLink theme={AppLinkTheme.SECONDARY} to="/" className={cls.mainLink}>Home</AppLink>
-            <AppLink theme={AppLinkTheme.SECONDARY} to="about">About</AppLink>
+export const Navbar = ({ className }: NavbarProps) => {
+    const { t } = useTranslation();
+    return (
+        <div className={classNames(cls.Navbar, {}, [className])}>
+            <div className={cls.links}>
+                <AppLink theme={AppLinkTheme.SECONDARY} to="/" className={cls.mainLink}>{t('HOME')}</AppLink>
+                <AppLink theme={AppLinkTheme.SECONDARY} to="about">{t('ABOUT')}</AppLink>
+            </div>
         </div>
-    </div>
-);
+    );
+};
